25c355b09383fc93313b6cdab4824f954417e52a
RufinaM03 <rufinajoachim1810@gmail.com>
2026-06-18 17:11:18 +0530

    feat(auth): implement signup, login validation and password confirmation

--- imports/ui/Login.js
+++ imports/ui/Login.js
@@ -1,24 +1,46 @@
 import { Meteor } from "meteor/meteor";
 import { Template } from "meteor/templating";
 import { ReactiveVar } from "meteor/reactive-var";
-
 import { Accounts } from "meteor/accounts-base";
 
 import "./Login.html";
 
 Template.login.onCreated(function () {
   this.isSignup = new ReactiveVar(false);
+  this.errorMessage = new ReactiveVar("");
 });
 
 Template.login.helpers({
+  isSignup() {
+    return Template.instance().isSignup.get();
+  },
+
+  pageTitle() {
+    return Template.instance().isSignup.get() ? "Create Account" : "TaskFlow";
+  },
+
+  pageSubtitle() {
+    return Template.instance().isSignup.get()
+      ? "Create your workspace and start organizing."
+      : "Organize Your Work, Your Way!";
+  },
+
   buttonText() {
     return Template.instance().isSignup.get() ? "Create Account" : "Log In";
   },
 
   switchText() {
     return Template.instance().isSignup.get()
-      ? "Already have an account? Log In"
-      : "Don't have an account? Create One";
+      ? "Already have an account?"
+      : "Don't have an account?";
+  },
+
+  switchActionText() {
+    return Template.instance().isSignup.get() ? "Log In" : "Create One";
+  },
+
+  errorMessage() {
+    return Template.instance().errorMessage.get();
   },
 });
 
@@ -26,41 +48,61 @@ Template.login.events({
   "submit .login-form"(event, instance) {
     event.preventDefault();
 
+    instance.errorMessage.set("");
+
     const { target } = event;
 
-    const username = target.username.value;
-
+    const username = target.username.value.trim();
     const password = target.password.value;
 
+    if (!username || !password) {
+      instance.errorMessage.set("Please enter both username and password.");
+      return;
+    }
+
     if (instance.isSignup.get()) {
+      const confirmPassword = target.confirmPassword.value;
+
+      if (password.length < 6) {
+        instance.errorMessage.set("Password must be at least 6 characters.");
+        return;
+      }
+
+      if (password !== confirmPassword) {
+        instance.errorMessage.set("Passwords do not match.");
+        return;
+      }
+
       Accounts.createUser(
         {
           username,
-
           password,
         },
         (err) => {
           if (err) {
-            alert(err.reason);
+            instance.errorMessage.set(err.reason);
+            return;
           }
+
+          target.reset();
         },
       );
-    } else {
-      Meteor.loginWithPassword(
-        username,
 
-        password,
-
-        (err) => {
-          if (err) {
-            alert(err.reason);
-          }
-        },
-      );
+      return;
     }
+
+    Meteor.loginWithPassword(username, password, (err) => {
+      if (err) {
+        instance.errorMessage.set("Invalid username or password.");
+        return;
+      }
+
+      target.reset();
+    });
   },
 
-  "click .switch-mode"(event, instance) {
+  "click .switch-mode span"(event, instance) {
+    instance.errorMessage.set("");
     instance.isSignup.set(!instance.isSignup.get());
   },
 });
